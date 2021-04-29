55fbf30b53bd34699a13e6bbf8c9217547d42dfa
Raphael Nascimento <raphael.nascimento@sp.senai.br>
2021-04-29 08:13:27 -0300

    update libraries

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: " http://192.168.1.100:3333",
+   baseURL: " http://192.168.137.29:3333",
    //  baseURL: "http://192.168.1.4:333",
-   // baseURL: "http://localhost:333",
+   // baseURL: "http://localhost:3333",
 });
 
 export default api;
